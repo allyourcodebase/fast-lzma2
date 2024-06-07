f7a790345fa7bd1868161c831be87a8168f31338
Jonathan Marler <johnnymarler@gmail.com>
2024-06-07 13:03:38 -0600

    zig update 0.13.0

--- build.zig
+++ build.zig
@@ -9,8 +9,10 @@ pub fn build(b: *std.Build) void {
     )) |multitarget| {
         var it = std.mem.split(u8, multitarget, ",");
         while (it.next()) |target_string| {
-            const target = std.zig.CrossTarget.parse(.{ .arch_os_abi = target_string }) catch |err|
-                std.debug.panic("failed to parse target '{s}' with {s}", .{target_string, @errorName(err)});
+            const target = b.resolveTargetQuery(
+                std.zig.CrossTarget.parse(.{ .arch_os_abi = target_string }) catch |err|
+                    std.debug.panic("failed to parse target '{s}' with {s}", .{target_string, @errorName(err)})
+            );
             const name = std.mem.concat(b.allocator, u8, &.{"fast-lzma2-", target_string}) catch @panic("OOM");
             addLib(b, name, target, optimize);
         }
@@ -23,7 +25,7 @@ pub fn build(b: *std.Build) void {
 fn addLib(
     b: *std.Build,
     name: []const u8,
-    target: std.zig.CrossTarget,
+    target: std.Build.ResolvedTarget,
     optimize: std.builtin.Mode,
 ) void {
     const lib = b.addStaticLibrary(.{
@@ -31,31 +33,34 @@ fn addLib(
         .target = target,
         .optimize = optimize,
     });
-    lib.addCSourceFiles(&[_][]const u8 {
-        "dict_buffer.c",
-        "fl2_common.c",
-        "fl2_compress.c",
-        "fl2_decompress.c",
-        "fl2_pool.c",
-        "fl2_threading.c",
-        "lzma2_dec.c",
-        "lzma2_enc.c",
-        "radix_bitpack.c",
-        "radix_mf.c",
-        "radix_struct.c",
-        "range_enc.c",
-        "util.c",
-        "xxhash.c",
-        }, &[_][]const u8 {
-        "-Wall",
-        "-O2",
-        "-pthread",
-        // note: project should be fixed to not need this
-        "-fno-sanitize=undefined",
+    lib.addCSourceFiles(.{
+        .files = &[_][]const u8 {
+            "dict_buffer.c",
+            "fl2_common.c",
+            "fl2_compress.c",
+            "fl2_decompress.c",
+            "fl2_pool.c",
+            "fl2_threading.c",
+            "lzma2_dec.c",
+            "lzma2_enc.c",
+            "radix_bitpack.c",
+            "radix_mf.c",
+            "radix_struct.c",
+            "range_enc.c",
+            "util.c",
+            "xxhash.c",
+        },
+        .flags = &[_][]const u8 {
+            "-Wall",
+            "-O2",
+            "-pthread",
+            // note: project should be fixed to not need this
+            "-fno-sanitize=undefined",
+        },
     });
     lib.linkLibC();
-    lib.installHeader("fast-lzma2.h", "fast-lzma2.h");
-    lib.installHeader("fl2_errors.h", "fl2_errors.h");
+    lib.installHeader(b.path("fast-lzma2.h"), "fast-lzma2.h");
+    lib.installHeader(b.path("fl2_errors.h"), "fl2_errors.h");
     b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
@@ -63,9 +68,9 @@ fn addLib(
         .target = target,
         .optimize = optimize,
     });
-    exe.addCSourceFiles(&[_][]const u8 {
+    exe.addCSourceFiles(.{ .files = &[_][]const u8 {
         "cmdline_tool.c",
-    }, &.{});
+    }});
     exe.linkLibrary(lib);
     b.installArtifact(exe);
 }
